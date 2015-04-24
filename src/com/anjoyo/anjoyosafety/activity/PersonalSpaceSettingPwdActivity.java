4b4c994db4e5d5d00eb7bb3911fdf868ae5f828a
wangyl521 <18801037781@163.com>
2015-04-24 09:45:57 +0800

    首次提交到本地仓库

---
+++ src/com/anjoyo/anjoyosafety/activity/PersonalSpaceSettingPwdActivity.java
@@ -0,0 +1,30 @@
+package com.anjoyo.anjoyosafety.activity;
+
+import android.os.Handler;
+
+import com.anjoyo.anjoyosafety.base.MyBaseActivity;
+import com.anjoyo.anjoyosatety.activity.R;
+import com.zdp.aseo.content.AseoZdpAseo;
+
+public class PersonalSpaceSettingPwdActivity extends MyBaseActivity {
+	public static Handler handler;
+	@Override
+	protected void controll() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void findViewById() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void setContentView() {
+		// TODO Auto-generated method stub
+setContentView(R.layout.passwrod);
+AseoZdpAseo.initType(this,AseoZdpAseo.SCREEN_TYPE);
+	}
+
+}
